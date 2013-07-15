09cd1cd25a3d4aeb56bca66e0c4522dd7163224d
Mathias Bak <bufas7@gmail.com>
2013-07-15 17:43:53 +0200

    Change colors and remove color/background changers

--- javascripts/scripts.js
+++ javascripts/scripts.js
@@ -25,25 +25,4 @@ $(document).ready(function() {
 	 	speed: 100,
 	 	touchDevices: false
 	 });
-	 
-	 $('.picker').colorpicker({ colorFormat: '#HEX', draggable: false, select: newcolor });
-	 
-	 $('.bgchang').click(function(){
-		 $('body').css('background-image','url('+$(this).attr('src')+')');
-	 });
 });
-
-
-
-// Change colors
-function newcolor(color) {
-	color = $('#picker').val();
- 	from = $(".navi").css("background-color");
-	$('*').filter(function(i){ return $(this).css("background-color") == from; }).css("background-color", color);
-	$('*').filter(function(i){ return $(this).css("border-color") == from; }).css("border-color", color);
-	$('*').filter(function(i){ return $(this).css("border-top-color") == from; }).css("border-top-color", color);
-	$('*').filter(function(i){ return $(this).css("border-bottom-color") == from; }).css("border-bottom-color", color);
-	$('*').filter(function(i){ return $(this).css("color") == from; }).css("color", color);
-	$('.tooltipster-default').css('background-color', color);
-	$('.resume-large-icon').remove();
-}
